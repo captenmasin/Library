83a6bc748d52571f0e37c6b655ba8b85a1b8901e
Mason Day <daymason@hotmail.co.uk>
2025-07-02 15:31:33 +0100

    WIP

--- resources/js/enums/UserBookStatus.ts
+++ resources/js/enums/UserBookStatus.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-unused-vars */
 export enum UserBookStatus {
-PlanToRead = 'Plan to Read',
-Reading = 'Reading',
-Completed = 'Completed',
-OnHold = 'On Hold',
-Dropped = 'Dropped'
+PlanToRead = "Plan to Read",
+Reading = "Reading",
+Completed = "Completed",
+OnHold = "On Hold",
+Dropped = "Dropped"
 }
 /* eslint-disable no-unused-vars */
