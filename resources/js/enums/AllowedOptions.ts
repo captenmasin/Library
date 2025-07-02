2f90b6fdbf64f494e7f4fa6d766ebc8f195e2f45
Mason Day <daymason@hotmail.co.uk>
2025-07-02 15:43:32 +0100

    WIP

--- resources/js/enums/AllowedOptions.ts
+++ resources/js/enums/AllowedOptions.ts
@@ -1,17 +1,17 @@
 /* eslint-disable no-unused-vars */
 export enum AllowedOptions {
-Width = 'width',
-Height = 'height',
-Format = 'format',
-Quality = 'quality',
-Blur = 'blur',
-Contrast = 'contrast',
-Flip = 'flip',
-Version = 'version',
-Background = 'background',
-Scale = 'scale',
-Crop = 'crop',
-Pixelate = 'pixelate',
-Rotate = 'rotate'
+Width = "width",
+Height = "height",
+Format = "format",
+Quality = "quality",
+Blur = "blur",
+Contrast = "contrast",
+Flip = "flip",
+Version = "version",
+Background = "background",
+Scale = "scale",
+Crop = "crop",
+Pixelate = "pixelate",
+Rotate = "rotate"
 }
 /* eslint-disable no-unused-vars */
