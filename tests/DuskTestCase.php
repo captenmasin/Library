6ea7a9f99c2190c69f8364bc7b47218c9ebb9e45
Mason Day <daymason@hotmail.co.uk>
2025-07-05 21:06:12 +0100

    WIP

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -38,7 +38,7 @@ abstract class DuskTestCase extends BaseTestCase
             $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
             '--disable-search-engine-choice-screen',
             '--disable-smooth-scrolling',
-            '--user-data-dir=/tmp/chrome-data-'.uniqid(),
+            '--user-data-dir='.sys_get_temp_dir().'/chrome-profile-'.getmypid(),
         ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
             return $items->merge([
                 '--disable-gpu',
