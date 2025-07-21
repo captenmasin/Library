729cdf4a8efe1ce1836ec0a0814f2b2c35d7d4f8
Mason Day <daymason@hotmail.co.uk>
2025-07-21 10:28:42 +0100

    WIP

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -15,10 +15,12 @@ defineProps<{
 <template>
     <div class="relative grid flex-col items-center bg-background justify-center px-8 h-dvh sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div class="relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex">
-            <div class="absolute inset-4 bg-secondary text-secondary-foreground rounded-xl" />
+            <div
+                style="background-image: url('https://images.unsplash.com/photo-1585521550659-64a7cafb39e2?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"
+                class="absolute inset-4 bg-cover bg-center bg-secondary text-secondary-foreground rounded-xl" />
             <Link
                 :href="useRoute('user.books.index')"
-                class="relative z-20 flex items-center text-2xl text-primary font-serif font-semibold">
+                class="relative z-20 flex items-center text-2xl text-white font-serif font-semibold tracking-tight">
                 <AppLogoIcon class="mr-2 rounded-lg fill-current size-8" />
                 {{ name }}
             </Link>
