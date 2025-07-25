932eac9d90fdf21eddf5caab5efc94c6b33378eb
Mason Day <daymason@hotmail.co.uk>
2025-07-25 10:32:54 +0100

    WIP

--- resources/js/components/books/BarcodeScanner.vue
+++ resources/js/components/books/BarcodeScanner.vue
@@ -36,10 +36,10 @@ async function startScan () {
     book.value = null
     scanning.value = true
 
-    useRequest(useRoute('api.books.fetch_or_create', '9780307763051'), 'GET')
-        .then(response => {
-            book.value = response.book
-        })
+    // useRequest(useRoute('api.books.fetch_or_create', '9780307763051'), 'GET')
+    //     .then(response => {
+    //         book.value = response.book
+    //     })
 
     try {
         controls = await codeReader.decodeFromConstraints(
