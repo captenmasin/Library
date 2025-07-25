6a412408a3b71beee96c954ce47bd38d8ed81069
Mason Day <daymason@hotmail.co.uk>
2025-07-25 16:12:01 +0100

    WIP

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
 >
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 
     <title>{!! $pageTitle !!}</title>
 
