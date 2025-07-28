fa8ff0b950a0c8747632356ea69dbe4eb6a2f32a
Mason Day <daymason@hotmail.co.uk>
2025-07-28 15:47:12 +0100

    WIP

--- resources/js/layouts/app/AppHeaderLayout.vue
+++ resources/js/layouts/app/AppHeaderLayout.vue
@@ -2,38 +2,54 @@
 import AppShell from '@/components/AppShell.vue'
 import AppHeader from '@/components/AppHeader.vue'
 import AppContent from '@/components/AppContent.vue'
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from '@/composables/useRoute'
 import { Link, router, usePage } from '@inertiajs/vue3'
 import type { BreadcrumbItemType, NavItem } from '@/types'
 import { useIsCurrentUrl } from '@/composables/useIsCurrentUrl'
 import { Home, LibraryBig, PlusSquareIcon } from 'lucide-vue-next'
-import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
 
 const page = usePage()
 const breadcrumbs = ref(page.props.breadcrumbs as BreadcrumbItemType[] | undefined)
 
-const mainNavItems: NavItem[] = [
+const mainNavItems = ref([
     {
         title: 'Home',
         href: useRoute('home'),
-        icon: Home
+        icon: Home,
+        isActive: false
     },
     {
         title: 'Library',
         href: useRoute('user.books.index'),
-        icon: LibraryBig
+        icon: LibraryBig,
+        isActive: false
     },
     {
         title: 'Add Books',
         href: useRoute('books.search'),
-        icon: PlusSquareIcon
+        icon: PlusSquareIcon,
+        isActive: false
     }
-]
+])
 
 const activeItemStyles = computed(
-    () => (url: string) => (useIsCurrentUrl(url) ? 'text-primary bg-primary/10' : '')
+    () => (item: NavItem) => (item.isActive ? 'text-primary bg-primary/10' : '')
 )
+
+onMounted(() => {
+    // Set active state for main navigation items based on current URL
+    mainNavItems.value.forEach(item => {
+        item.isActive = useIsCurrentUrl(item.href)
+    })
+})
+
+function handleClick (item: NavItem) {
+    // Update the active state when a navigation item is clicked
+    mainNavItems.value.forEach(navItem => {
+        navItem.isActive = (navItem.href === item.href)
+    })
+}
 
 router.on('navigate', (event) => {
     const newBreadcrumbs = event.detail.page.props.breadcrumbs as BreadcrumbItemType[] | undefined
@@ -62,8 +78,9 @@ router.on('navigate', (event) => {
                     <Link
                         :href="item.href"
                         prefetch
-                        :class="[activeItemStyles(item.href)]"
-                        class="flex py-3 w-full rounded-xl items-center justify-center text-sm text-foreground hover:text-primary">
+                        :class="[activeItemStyles(item)]"
+                        class="flex py-3 w-full rounded-xl items-center justify-center text-sm text-foreground hover:text-primary"
+                        @click="handleClick(item)">
                         <component
                             :is="item.icon" />
                     </Link>
