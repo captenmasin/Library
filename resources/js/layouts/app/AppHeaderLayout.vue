e00cd6d2a57fce9d79a8d18e2fb47dbb3e401cfd
Mason Day <daymason@hotmail.co.uk>
2025-07-28 18:33:19 +0100

    WIP

--- resources/js/layouts/app/AppHeaderLayout.vue
+++ resources/js/layouts/app/AppHeaderLayout.vue
@@ -81,7 +81,7 @@ router.on('navigate', (event) => {
                         @click="handleClick(item)">
                         <div
                             :class="[item.isActive ? 'bg-primary/10' : 'bg-transparent']"
-                            class="rounded-full px-4 sm:px-0 sm:py-0 py-1 transition-all">
+                            class="rounded-full px-5 sm:px-0 sm:py-0 py-2 transition-all">
                             <component
                                 :is="item.icon"
                                 class="size-5 sm:size-4" />
